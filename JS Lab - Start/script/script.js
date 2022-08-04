b5b5c201f3701c8198ff596e1aedd25f8dabaafb
300363740 <110199182+300363740@users.noreply.github.com>
2022-08-04 09:45:56 +0530

    Update script.js

--- JS Lab - Start/script/script.js
+++ JS Lab - Start/script/script.js
@@ -6,7 +6,6 @@ function addValue(){
 
     document.mycalculator.output.value = firstOperand + '+' + secondOperand + '=' + output;
 }
-C:\Users\jaska\Downloads\JS Lab - Start\JS Lab - Start\script\script.js
 
 function mulValue(){
     var firstOperand=parseInt(document.querySelector('#input_a').value);
